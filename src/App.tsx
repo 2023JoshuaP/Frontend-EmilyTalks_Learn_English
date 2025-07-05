914d1fd46e91354470b7b869973f8858a6c528c4
2023JoshuaP <jphilco@unsa.edu.pe>
2025-07-05 17:40:16 -0500

    Control main of the application.

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Mode from "./pages/Mode";
+import Topic from "./pages/Topic";
+import Scenario from "./pages/Scenario";
+import Conversation from "./pages/Conversation";
+import Report from "./pages/Report";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/welcome" replace />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/mode" element={<Mode />} />
+          <Route path="/topic" element={<Topic />} />
+          <Route path="/scenario" element={<Scenario />} />
+          <Route path="/conversation" element={<Conversation />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
